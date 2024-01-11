b5ad85dc6e3f5216d8e1374d10e4beacfada5d89
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-11 17:13:22 -0500

    initial commit-- setting up folder structure

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuDZFNr2xHHBmgay5dBi-uZxeFijfMpE8",
+    authDomain: "netflix-clone-a5754.firebaseapp.com",
+    projectId: "netflix-clone-a5754",
+    storageBucket: "netflix-clone-a5754.appspot.com",
+    messagingSenderId: "464140795934",
+    appId: "1:464140795934:web:6f0d256ecf06e081126e0f"
+  };
