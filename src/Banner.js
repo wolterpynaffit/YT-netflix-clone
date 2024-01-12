880564631f7bef588dbb9d371fe891bed5739acd
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-12 15:44:56 -0500

    added banner image

--- src/Banner.js
+++ src/Banner.js
@@ -3,7 +3,12 @@ import "./Banner.css"
 
 function Banner() {
   return (
-    <div>Banner</div>
+    <header className = "banner" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      backgroundImage:`url('https://s3.amazonaws.com/www.explorersweb.com/wp-content/uploads/2022/09/08171426/Broad-Peak-banner-1-e1662657343870.jpg')`}}>
+  
+    </header>
   )
 }
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./HomeScreen.css";
 import Nav from "./Nav"
+import Banner from './Banner';
 
 
 function HomeScreen() {
