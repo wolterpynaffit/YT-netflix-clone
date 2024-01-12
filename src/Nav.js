bddc5f636220677c88a65372615c3c41b0f0cbbc
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-11 20:00:42 -0500

    more styling

--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,15 @@ import React from 'react';
 import "./Nav.css";
 
 function Nav() {
+    const [show, handleShow] = useState(false)
+
+
+    // This code snippet defines a function called transitionNavBar. It checks if the vertical scroll position of the window is greater than 100 pixels. If it is, it calls another function called handleShow with the argument true.
+    const transitionNavBar = () => {
+        if (window.scrollY > 100){
+            handleShow(true)
+        }
+    }
   return (
     <div className ="nav nav_black">
         <div className="nav_contents">
