886e4c705c36d1c06816e213b8bc1c9540e01e9f
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-11 19:02:54 -0500

    project setup

---
+++ src/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className = "homeScreen">
+        {/* Nav */}
+        {/* Banner */}
+        {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className ="nav"> Nav</div>
+  )
+}
+
+export default Nav
