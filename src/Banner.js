50491030072d1e2e38750f238f8a0b8ba13c87c7
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-14 22:06:56 -0500

    Live banner, movie titles and description

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Banner.css"
+import axios from './axios';
+import requests from './app/Requests';
 
 function Banner() {
+
+  const [movie, setMovie]= useState([]);
+
+// This is going to fetch the movie information
+// This code snippet defines an asynchronous function called fetchData. It uses the axios library to make an HTTP GET request to the fetchNetflixOriginals endpoint. The response data is stored in the request variable. Then, it selects a random movie from the response data and sets it using the setMovie function. Finally, it returns the request object.
+  useEffect(()=> {
+    async function fetchData(){
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      )
+      return request;
+    }
+    fetchData()
+  },[]) // empty dependency so that this code is only fired when the banner component mounts 
+  console.log(movie)
+  console.log('BACKDROP_PATH:' + movie.backdrop_path)
+  console.log('FIRST_AIR_DATE: ' +  movie.first_air_date)
+  console.log('MOVIE_NAME:' + movie.name)
   function truncate(string, n){
     // below is a ternary operator that checks to see if the length of the string is greater than 'n'. If the condition is true the first code snippit will run, adding an ellipses to the end of the string. Otherwise the second code snippit will run, which returns the full length of the string itself. 
     return string?.length > n ? string.substring(0, n-1) + '...' : string
@@ -9,20 +32,25 @@ function Banner() {
 
   }
   return (
-    <header className = "banner" style={{
+    <header className = "banner"
+     style={{
       backgroundSize: 'cover',
       backgroundPosition: 'center center',
       // ensures the image remains central vertically and horizontally 
-      backgroundImage:`url('https://wallpapercave.com/dwp2x/wp8741227.jpg')`}}>
+      backgroundImage:`url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`
+      // the interpolation is movie(optionial)
+    }}
+    >
         <div className="banner_contents">
-          <h1 className="banner_title">
-            Movie Name
-          </h1>
+          <h1 className="banner_title"> {movie?.title || movie?.name || movie?.original_name}</h1>
           <div className="banner_buttons">
             <button className = "banner_button">Play</button>
             <button className = "banner_button">My List</button>
           </div>
-          <h1 className = "banner_description"> {truncate(`This is the descriptionThis project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template. l`, 250)} </h1>
+
+          <h1 className = "banner_description"> {truncate(movie?.overview, 250)} </h1>
+
+
         </div>
         <div className = "banner--fadeBottom"/>
 
--- src/app/Requests.js
+++ src/app/Requests.js
@@ -1,7 +1,9 @@
+// Typically we would store the api key in {process.ev.API_KEY}
 const API_KEY = '096bc391d7771398af6a6919d15acc37';
 
 
 // because we've installed and created a axios.js with the base url, the api call will use the following request structure to essentially to "fill in the blank": The API call will ultimately look something like this: "https://api.themoviedb.org/3/trending/all/week?api_key=096bc391d7771398af6a6919d15acc37&with_genres=99" 
+
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
@@ -13,3 +15,4 @@ const requests = {
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
 }
 
+export default requests;
