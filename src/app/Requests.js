a466d380fa31a3f0ad448efb098059d58942f1ca
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-17 15:27:41 -0500

    rewrote code in attempt to map movies

--- src/app/Requests.js
+++ src/app/Requests.js
@@ -1,8 +1,8 @@
 // Typically we would store the api key in {process.ev.API_KEY}
-const API_KEY = '096bc391d7771398af6a6919d15acc37';
+const API_KEY = 'faecfa97c656fff3de6165e36d71d11c';
 
 
-// because we've installed and created a axios.js with the base url, the api call will use the following request structure to essentially to "fill in the blank": The API call will ultimately look something like this: "https://api.themoviedb.org/3/trending/all/week?api_key=096bc391d7771398af6a6919d15acc37&with_genres=99" 
+// because we've installed and created a axios.js with the base url, the api call will use the following request structure to essentially to "fill in the blank": The API call will ultimately look something like this: "https://api.themoviedb.org/3/trending/all/week?api_key=faecfa97c656fff3de6165e36d71d11c&with_genres=99" 
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
