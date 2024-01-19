c236ff18ea47d40f0c2ff1205c996d33dcd0d515
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-18 20:41:33 -0500

    preventing broken images from displaying

--- src/Row.js
+++ src/Row.js
@@ -60,56 +60,33 @@ const Row = ({title, fetchUrl, isLargeRow = false}) => {
   }, [fetchUrl]);
 
 
+
   return (
     <div className = "row">
-      <h2> {title}</h2>
-
-<div className='row_posters'>
-{movies.length > 0 ? (
-      movies.map((movie) => (
-        <img
-        className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
-          key={movie.id}
-          src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-          alt={movie.name}
-        />
-      ))
-    ) : (
-      <p>No movies available</p>
-    )}
-
+      <h2> {title} </h2> 
+      <div className = " row_posters">
+        {movies.map(
+          (movie) => 
+          (isLargeRow && movie.poster_path) ||
+          (!isLargeRow && movie.backdrop_path && ( 
+            <img 
+            className ={`row_poster  ${isLargeRow && "row_posterLarge"}`}
+            key = {movie.id}
+            src={`$base_url${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            />
+          )) 
+        )}
+      </div>
     </div>
-</div>
-
-  )
+  
+  );
 }
 
 export default Row
 
 
-
-
-//     return (
-//     <div className="row">
-//         <h2>{title}</h2>
         
-//         <div className="row_posters">
-//             {movies.map(
-//                 (movie) =>
-//                     ((isLargeRow && movie.poster_path) || 
-//                     (!isLargeRow && movie.backdrop_path)) && (
-//                     <img
-//                         className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-//                         key={movie.id} 
-//                         src={`${base_url}${
-//                         isLargeRow ? movie.poster_path : movie.backdrop_path
-//                         }`} 
-//                         alt={movie.name}
-//                     />
-//                     ))}
-//         </div>
-//     </div>
-//     );
-// }
 
-// export default Row;
