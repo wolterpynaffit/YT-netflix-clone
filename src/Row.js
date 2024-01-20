a253407b736d343b331cdad13db47928dc9b3f48
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2024-01-20 13:09:40 -0500

    fixed movie poster display

--- src/Row.js
+++ src/Row.js
@@ -60,33 +60,33 @@ const Row = ({title, fetchUrl, isLargeRow = false}) => {
   }, [fetchUrl]);
 
 
-
   return (
     <div className = "row">
-      <h2> {title} </h2> 
-      <div className = " row_posters">
+      <h2> {title}</h2>
+
+      <div className='row_posters'>
+
         {movies.map(
-          (movie) => 
-          (isLargeRow && movie.poster_path) ||
-          (!isLargeRow && movie.backdrop_path && ( 
-            <img 
-            className ={`row_poster  ${isLargeRow && "row_posterLarge"}`}
-            key = {movie.id}
-            src={`$base_url${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
+          (movie) => (
+          // this prevents deadlinks from displaying | Initially making this change broke something in the application, the Large poster images disappeared and a string of random characters appeared instead. In order to fix this we need to wrap the expression in parenthesis:
+
+          //    (isLargeRow && movie.poster_path) || 
+          //(!isLargeRow && movie.backdrop_path && (THIS IS WHAT MADE IT BREAK) 
+          ((isLargeRow && movie.poster_path) || 
+          (!isLargeRow && movie.backdrop_path)) && (
+            <img
+            className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
+              key={movie.id}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
             />
-          )) 
-        )}
+          )
+      ),)}
       </div>
-    </div>
-  
+</div>
+
   );
 }
 
 export default Row
-
-
-        
 
